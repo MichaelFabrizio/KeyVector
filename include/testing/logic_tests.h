5f50b90b60057dace4dd942bdc570d5fcaae0751
mfabrizio <83029804+MichaelFabrizio@users.noreply.github.com>
2024-06-28 09:58:20 -0400

    [Refactor] Logic_Tests class now takes a KeyVector as a parameter instead of generating it internally. This more closely matches how systems will work in the future. [Fixes] Renamed Logic_Tests.h to logic_tests.h. (#7)

--- include/testing/logic_tests.h
+++ include/testing/logic_tests.h
@@ -26,10 +26,10 @@ class Logic_Tests {
 	// Checks that the test_key landed at a specified inner location ( 2nd possibility)
 	bool Confirm_Key_At_Index_Is_Valid(const Key test_key, const Key index) {
 		// INDEX TESTING CRITERIA:
-		if (index != castles->FindIndex(test_key)) { return false; }
+		if (index != testkeyvector.FindIndex(test_key)) { return false; }
 
 		// VALUE TESTING CRITERIA:
-		auto& castle = castles->Find(test_key);
+		auto& castle = testkeyvector.Find(test_key);
 		if (castle.damage_level != 2525)	{ return false; }
 		if (castle.armor_level != 3636)		{ return false; }
 		
@@ -38,13 +38,13 @@ class Logic_Tests {
 	
 	// Checks that the test_key landed on the end location ( 1st possibility)
 	bool Confirm_Key_At_End_Is_Valid(const Key test_key) {
-		return Confirm_Key_At_Index_Is_Valid(test_key, castles->Length());
+		return Confirm_Key_At_Index_Is_Valid(test_key, testkeyvector.Length());
 	}
 
 	// Checks if test_key is well-bounded, 
 	// And that it exists within the initial_keys vector
 	bool Is_Valid_Initial_Vector(std::vector<Key>& initial_keys, Key test_key) {
-		if (test_key >= castles->Capacity())	{ return false; }
+		if (test_key >= testkeyvector.Capacity())	{ return false; }
 
 		bool test_key_found = false;
 
@@ -59,15 +59,15 @@ class Logic_Tests {
 	bool Try_Add_Elements_Until_Zeroed_Next_Index(const Key test_key) {
 			while (true) {
 
-				auto Next = castles->Length() + 1;
+				auto Next = testkeyvector.Length() + 1;
 
 				if (Next == test_key)							{ return false; }
-				if (Next == castles->Capacity())	{ return false; }
+				if (Next == testkeyvector.Capacity())	{ return false; }
 
-				auto Next_Index = castles->FindIndex(Next);
+				auto Next_Index = testkeyvector.FindIndex(Next);
 				if (Next_Index == 0) { return true; }
 
-				else { castles->AddAny(); } // Continue to next loop
+				else { testkeyvector.AddAny(); } // Continue to next loop
 
 			}
 	}
@@ -76,15 +76,15 @@ class Logic_Tests {
 	bool Try_Add_Elements_Until_Active_Next_Index(const Key test_key) {
 			while (true) {
 
-				auto Next = castles->Length() + 1;
+				auto Next = testkeyvector.Length() + 1;
 
 				if (Next == test_key)							{ return false; }
-				if (Next == castles->Capacity())	{ return false; }
+				if (Next == testkeyvector.Capacity())	{ return false; }
 
-				auto Next_Index = castles->FindIndex(Next);
+				auto Next_Index = testkeyvector.FindIndex(Next);
 				if (Next_Index != 0) { return true; }
 
-				else { castles->AddAny(); } // Continue to next loop
+				else { testkeyvector.AddAny(); } // Continue to next loop
 
 			}
 	}
@@ -102,8 +102,7 @@ class Logic_Tests {
 	}
 
 public:
-	Logic_Tests() {
-		castles = &pool.AddKeyVec<Castle, unsigned char, 256>();
+	Logic_Tests(KeyVec& TestKeyVector) : testkeyvector(TestKeyVector) {
 	}
 
 	void Test_Add_Any() {
@@ -111,78 +110,78 @@ public:
 		// In this test: _indices[7] = _indices[_Length + 1] = 1 (it is indexed)
 
 		std::vector<std::size_t> test_keys { 7, 2, 3, 4, 5, 6 };
-		castles->BuildFromVector(test_keys);
+		testkeyvector.BuildFromVector(test_keys);
 		
-		auto added_key = castles->AddAny();
+		auto added_key = testkeyvector.AddAny();
 
-		if (castles->FindIndex(7) != 7) { _status = false; }
+		if (testkeyvector.FindIndex(7) != 7) { _status = false; }
 		else if (added_key != 1)				{ _status = false; }
 		
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Add_Any_2() {
 		std::vector<std::size_t> test_keys { 7, 2, 3, 45, 5, 6, 15, 25 };
-		castles->BuildFromVector(test_keys);
+		testkeyvector.BuildFromVector(test_keys);
 
 		Key added_key = 0;
 		while (true) {
-			auto location_45 = castles->FindIndex(45);
+			auto location_45 = testkeyvector.FindIndex(45);
 
 			if (location_45 != 4) {
-				if (added_key != 4) { _status = false; castles->Debug(); }
-				castles->Clear();
+				if (added_key != 4) { _status = false; }
+				testkeyvector.Clear();
 				return;
 			}
 			else {
-				added_key = castles->AddAny();
+				added_key = testkeyvector.AddAny();
 			}
 		}
 	}
 
 	void Test_Add_Any_3() {
 		for (int i = 0; i < 10; i++) {
-			castles->AddAny();
+			testkeyvector.AddAny();
 		}
 		
 		for (int i = 0; i < 10; i++) {
-			if (castles->FindIndex(i) != i) { _status = false; }
+			if (testkeyvector.FindIndex(i) != i) { _status = false; }
 		}
 		
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Add_Greater_Key_Branch_1() {
-		castles->AddRange(1, 10);
-		castles->Add(13);
+		testkeyvector.AddRange(1, 10);
+		testkeyvector.Add(13);
 
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
 		if (indices[10] != 13) { _status = false; }
 		if (indices[13] != 10) { _status = false; }
 
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Add_Greater_Key_Branch_2() {
-		castles->Add(13);
-		castles->AddRange(2, 13);
-		castles->Add(25);
+		testkeyvector.Add(13);
+		testkeyvector.AddRange(2, 13);
+		testkeyvector.Add(25);
 
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
 		if (indices[13] != 13)	{ _status = false; }
 		if (indices[25] != 1)		{ _status = false; }
 		if (indices[1] != 25)		{ _status = false; }
 
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 
 	void Test_Add_Equal_Key() { 
-		castles->AddRange(1, 128);
+		testkeyvector.AddRange(1, 128);
 
 		for (int i = 1; i < 128; i++) {
-			std::size_t index = castles->FindIndex(i);
+			std::size_t index = testkeyvector.FindIndex(i);
 			
 
 			// Test succeeded for this i value 
@@ -190,63 +189,63 @@ public:
 			// Test has failed
 			else { _status = false; }
 		}
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 
 	void Test_Add_Lesser_Key_Branch_1() {
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
-		castles->AddRange(1, 6);
-		castles->Add(25);
-		castles->AddRange(7, 25);
-		castles->Add(6);
+		testkeyvector.AddRange(1, 6);
+		testkeyvector.Add(25);
+		testkeyvector.AddRange(7, 25);
+		testkeyvector.Add(6);
 
 		if (indices[6] != 6)		{ _status = false; }
 		if (indices[25] != 25)	{ _status = false; }
 		
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Add_Lesser_Key_Branch_2() {
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
-		castles->AddRange(1, 6);
-		castles->Add(25);
-		castles->Add(6);
+		testkeyvector.AddRange(1, 6);
+		testkeyvector.Add(25);
+		testkeyvector.Add(6);
 		
 		if (indices[6] != 6)	{ _status = false; }
 		if (indices[7] != 25) { _status = false; }
 		if (indices[25] != 7) { _status = false; }
 		
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Add_Lesser_Key_Branch_3() {
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
-		castles->AddRange(1, 6);
-		castles->Add(25);
-		castles->Add(17);
-		castles->AddRange(8, 17);
-		castles->Add(6);
+		testkeyvector.AddRange(1, 6);
+		testkeyvector.Add(25);
+		testkeyvector.Add(17);
+		testkeyvector.AddRange(8, 17);
+		testkeyvector.Add(6);
 		
 		if (indices[6] != 6)	{ _status = false; }
 		if (indices[17] != 17) { _status = false; }
 		if (indices[25] != 7) { _status = false; }
 		if (indices[7] != 25) { _status = false; }
 		
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 
 	void Test_Clear() {
 		// Create an arbitrary KeyVector
 		std::vector<std::size_t> test_keys { 17, 2, 3, 45, 5, 6, 15, 25 };
-		castles->BuildFromVector(test_keys);
+		testkeyvector.BuildFromVector(test_keys);
 
-		castles->Clear();
+		testkeyvector.Clear();
 		
 		// Use GetIndexArray() method to get underlying _indices array
-		auto& indices = castles->GetIndexArray(); // Type: std::array<unsigned char, 256>&
+		auto& indices = testkeyvector.GetIndexArray(); // Type: std::array<unsigned char, 256>&
 		
 		// Every value in _indices must equal zero now.
 		for (auto index : indices) {
@@ -259,15 +258,15 @@ public:
 	// Test case: Remove(25). Where _indices[_length] == 25
 	void Test_Remove_Greater_Key_Branch_1() {
 		std::vector<std::size_t> test_keys { 17, 2, 3, 45, 5, 6, 15, 25 };
-		castles->BuildFromVector(test_keys);
-		auto& indices = castles->GetIndexArray(); // Type: std::array<unsigned char, 256>&
+		testkeyvector.BuildFromVector(test_keys);
+		auto& indices = testkeyvector.GetIndexArray(); // Type: std::array<unsigned char, 256>&
 		
-		auto index = castles->FindIndex(25);
-		auto& castle = castles->Find(25);
+		auto index = testkeyvector.FindIndex(25);
+		auto& castle = testkeyvector.Find(25);
 		castle.damage_level = 25;
 		castle.armor_level = 52;
 
-		castles->Remove(25);
+		testkeyvector.Remove(25);
 
 		if (indices[index] != 0) {
 			std::cout << "Endpoint not cleared, index: " << (std::size_t)indices[index] << '\n';
@@ -276,7 +275,7 @@ public:
 		if (indices[25] != 0) {
 			_status = false;
 		}
-		castles->Clear();
+		testkeyvector.Clear();
 		
 	}
 
@@ -284,11 +283,11 @@ public:
 		bool valid = Is_Valid_Initial_Vector(initial_keys, test_key);
 		if (!valid) { std::cout << "Invalid test key\n"; return; }
 		
-		castles->BuildFromVector(initial_keys);
-		auto& indices = castles->GetIndexArray(); // Type: std::array<unsigned char, 256>&
-		auto length = castles->Length();
+		testkeyvector.BuildFromVector(initial_keys);
+		auto& indices = testkeyvector.GetIndexArray(); // Type: std::array<unsigned char, 256>&
+		auto length = testkeyvector.Length();
 
-		Key test_key_index = castles->FindIndex(test_key);
+		Key test_key_index = testkeyvector.FindIndex(test_key);
 		Key end_key = indices[length];
 		
 		// MUST SKIP THIS POSSIBLE BRANCH FOR THIS TEST
@@ -301,57 +300,41 @@ public:
 		// Implicit: _indices[test_key] < _length
 		// Ready to begin test
 
-		castles->Remove(test_key);
+		testkeyvector.Remove(test_key);
 
 		if (indices[test_key_index] != end_key) { _status = false; }
 		if (indices[length] != 0)								{ _status = false; }
 		if (indices[test_key] != 0)							{ _status = false; }
 
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 
 	void Test_Remove_Lesser_Key_Branch_1() {
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 
-		castles->AddRange(1, 51);
+		testkeyvector.AddRange(1, 51);
 		
-		castles->Remove(28);
+		testkeyvector.Remove(28);
 
 		if (indices[28] != 50) { _status = false; }
 		if (indices[50] != 28) { _status = false; }
 
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 	
 	void Test_Remove_Lesser_Key_Branch_2() {
-		auto& indices = castles->GetIndexArray();
+		auto& indices = testkeyvector.GetIndexArray();
 		
-		castles->AddRange(1, 45);
-		castles->Add(50);
+		testkeyvector.AddRange(1, 45);
+		testkeyvector.Add(50);
 
-		castles->Remove(28);
+		testkeyvector.Remove(28);
 		
 		if (indices[28] != 50)	{ _status = false; }
 		if (indices[45] != 0)		{ _status = false; }
 		if (indices[50] != 28)	{ _status = false; }
 
-		castles->Clear();
-	}
-
-	void Test_Values() {
-		auto& indices = castles->GetIndexArray();
-		castles->AddRange(1, 11);
-
-		for (int i = 0; i < 8; i++) {
-			auto& castle = castles->Find(i);
-			castle.damage_level = i;
-		}
-
-		for (Castle& castle : *castles) {
-			std::cout << "Castle: " << castle.damage_level << '\n';
-		}
-
-		castles->Clear();
+		testkeyvector.Clear();
 	}
 
 	bool Return_Test_Status() {
@@ -359,8 +342,7 @@ public:
 	}
 
 private:
-	Pool<524288, 4096> pool;
-	KeyVector<Castle, unsigned char, 256>* castles;
+	KeyVec testkeyvector;
 
 	// Eventually change to a vector<string TestName, bool Result> (so we can store multiple failed tests)
 	bool _status = true;
